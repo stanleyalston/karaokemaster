09581dd36f9b9ea60d0b780cf8ab98ed6ef3f2e4
Stanley Alston <stanleyalston@hotmail.com>
2013-06-19 10:27:40 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karaokemaster::Application.config.secret_token = 'da150f03063dc133d1329af5ce9a088a3c416991fe5e26b168e0fc49889382f8ce67ef999227c272ec2a58af1ae58226496371dae4bb16c60bac23c439a39394'
